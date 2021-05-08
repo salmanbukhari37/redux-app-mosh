3df557c3167054616159910bfbe87c2c474cbe89
Salman Bukhari <salman@renegadefurniture.com>
2021-05-08 13:19:07 +0500

    funtional programming

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import pipe from "lodash/fp";
+import { Map } from 'immutable';
+import { produce } from "immer";
 
 function App() {
+  var book = { title: "Harry Potter" };
+
+  // immutable
+  // var book = Map({ title: "Harry Potter" });
+
+  // const publish = (book) => {
+  //   return book.set("isPublished", true);
+  // }
+
+  // let newbook = publish(book);
+  // // console.log(book.get("title"));
+  // console.log(newbook.toJS());
+
+  const publish = (book) => {
+    return produce(book, draftBook => {
+      draftBook.isPublished = true;
+    });
+  }
+
+  let updated = publish(book);
+
+  console.log(updated, book);
+
+  const str = "   Javascript   ";
+  const trim = str.trim();
+  const toLowerCase = str.toLowerCase();
+  const wrap = type => str => `<${type}>${str}</${type}>`;
+  const transform = pipe(trim, toLowerCase, wrap('div')(str));
+
   return (
     <div className="App">
       <header className="App-header">
